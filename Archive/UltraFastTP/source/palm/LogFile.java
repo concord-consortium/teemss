704a2a30063861b3cfc86e7b29d0fb3d80bfe5d9
scytacki <>
2001-07-05 01:52:34 +0000

    Updated make file system to support multiple platforms

---
+++ Archive/UltraFastTP/source/palm/LogFile.java
@@ -0,0 +1,15 @@
+import waba.io.*;
+import waba.ui.*;
+
+public class LogFile extends Transform
+{
+
+    public LogFile(String name)
+    {
+    }
+
+    public boolean transform(int num, int size, float [] data)
+    {
+    }
+
+}
