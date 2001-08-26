db21e09a75817497b7166b5b686e90d1f2d89e74
scytacki <>
2001-08-26 14:29:55 +0000

    passing votage on

---
+++ ProbeLib/source/probes/CCRawData.java
@@ -0,0 +1,41 @@
+package org.concord.waba.extra.probware.probs;
+import org.concord.waba.extra.event.DataListener;
+import org.concord.waba.extra.event.DataEvent;
+import extra.util.DataDesc;
+import org.concord.waba.extra.probware.*;
+import extra.util.*;
+
+public class CCRawData extends CCProb{
+float  			[]rawData = new float[1];
+float  			dtChannel = 0.0f;
+	CCRawData(){
+		this("unknown");
+	}
+	CCRawData(String name){
+		setName(name);
+		dDesc.setChPerSample(2);
+		dDesc.setDt(0.0f);
+		dEvent.setDataDesc(dDesc);
+		dEvent.setDataOffset(0);
+		dEvent.setNumbData(1);
+		dEvent.setData(rawData);
+
+		properties = new PropObject[1];
+		properties[0] = new PropObject(samplingModeString,samplingModes); 
+		setPropertyValue(0,samplingModes[CCProb.SAMPLING_10BIT_MODE]);
+
+		
+	}
+	public void setDataDescParam(int chPerSample,float dt){
+		dDesc.setDt(dt);
+		dDesc.setChPerSample(chPerSample);
+		dtChannel = dt / (float)chPerSample;
+	}
+
+    public boolean transform(DataEvent e)
+    {
+	System.out.println("Time: " + e.time + ", Val: " + e.data[e.dataOffset] + ", Ct: " + e.numbData);
+	    notifyListeners(e);
+	    return true;
+    }
+}
