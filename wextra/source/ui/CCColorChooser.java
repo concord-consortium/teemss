78eb568bd941360e6fcb4535bc0958034f200716
dima <>
2001-10-03 21:15:30 +0000

    *** empty log message ***

--- wextra/source/ui/CCColorChooser.java
+++ wextra/source/ui/CCColorChooser.java
@@ -1,48 +1,13 @@
 package org.concord.waba.extra.ui;
 
 public class CCColorChooser extends waba.ui.Control{
-waba.fx.Color	[]colors = new waba.fx.Color[256];
+waba.fx.Color	[]colors = null;
 public int colorIndex = -1;
 waba.fx.Image bufIm = null;
 int dx = 8;
 int dy = 6;
 	public CCColorChooser(){
-		int r,g,b,i;
-		int count = 0;
-		colors[count++] = new waba.fx.Color(0,0,0);
-		colors[count++] = new waba.fx.Color(255,255,255);
-		for(r = 0; r <=255; r += 51){
-			for(b = 0; b <=255; b += 51){
-				for(g = 0; g <=255; g += 51){
-					if(r == g && r == b && r == 0) continue;
-					if(r == g && r == b && r == 255) continue;
-					colors[count++] = new waba.fx.Color(r,g,b);
-				}
-			}
-		}
-
-		g = b = 0;
-		for(i = 1; i <= 10; i++){
-			r = (int)(255f*i/11f+0.5);
-			colors[count++] = new waba.fx.Color(r,g,b);
-		} 
-
-		r = b = 0;
-		for(i = 1; i <= 10; i++){
-			g = (int)(255f*i/11f+0.5);
-			colors[count++] = new waba.fx.Color(r,g,b);
-		} 
-
-		r = g = 0;
-		for(i = 1; i <= 10; i++){
-			b = (int)(255f*i/11f+0.5);
-			colors[count++] = new waba.fx.Color(r,g,b);
-		} 
-
-		for(i = 1; i <= 10; i++){
-			r = (int)(255f*i/11f+0.5);
-			colors[count++] = new waba.fx.Color(r,r,r);
-		} 
+		colors = CCPalette.getPalette();
 	}
 	
 	public void destroy(){
@@ -53,20 +18,22 @@ int dy = 6;
 	}
 	
 	public void setRect(int x,int y,int w,int h){
-		super.setRect(x,y,128,96);
+		super.setRect(x,y,130,86);
 	}
 	
 	public void createOffImage(){
 		if(bufIm != null) return;
 		bufIm=new waba.fx.Image(width,height);
 		 waba.fx.Graphics ig = new waba.fx.Graphics(bufIm);
-		int xx = 0;
-		int yy = 0;
+		int xx = 1;
+		int yy = 1;
+		ig.setColor(200,200,200);
+		ig.fillRect(xx,yy,width,height);
 		for(int i = 0; i < colors.length; i++){
 			ig.setColor(colors[i].getRed(),colors[i].getGreen(),colors[i].getBlue());
 			ig.fillRect(xx,yy,dx,dy);
 			if(((i+1) % 16) == 0){
-				xx = 0;
+				xx = 1;
 				yy += dy;
 			}else{
 				xx+=dx;
@@ -74,7 +41,12 @@ int dy = 6;
 		}
 		ig.setColor(0,0,0);
 		ig.drawRect(0,0,width,height);
-     		ig.free();
+		ig.drawLine(width,height-7,width-64,height - 7);
+		ig.drawLine(width-64,height - 7,width-64,height - 1);
+		ig.setColor(200,200,200);
+		ig.drawLine(width-63,height-1,width-1,height-1);
+		ig.drawLine(width-1,height-6,width-1,height - 1);
+    	ig.free();
 	}
 	
 	public void onPaint(waba.fx.Graphics g){
@@ -98,27 +70,31 @@ int dy = 6;
 	}
 
 	public void onEvent(waba.ui.Event event){
-		if (event.type == waba.ui.PenEvent.PEN_DOWN){
+		if (event.type == waba.ui.PenEvent.PEN_DOWN){		
 			waba.ui.PenEvent penEvent = (waba.ui.PenEvent)event;
-			int px = penEvent.x / dx;
-			int py = penEvent.y / dy;
+			int px = (penEvent.x - 1) / dx;
+			int py = (penEvent.y - 1) / dy;
 			if(px < 0 || px > 15) return;
 			if(py < 0 || py > 15) return;
 			int oldIndex = colorIndex;
 			colorIndex = py*16 + px;
+			if(colorIndex >= colors.length){
+				colorIndex = oldIndex;
+				return;
+			}
 			waba.fx.Graphics g =  createGraphics();
 			if(g == null){
 				repaint();
 				return;
 			}else{
 				if(oldIndex >= 0){
-					int xOld = dx * (oldIndex % 16);
-					int yOld = dy * (oldIndex / 16);
+					int xOld = 1 + dx * (oldIndex % 16);
+					int yOld = 1 + dy * (oldIndex / 16);
 					g.setColor(colors[oldIndex].getRed(),colors[oldIndex].getGreen(),colors[oldIndex].getBlue());
 					g.fillRect(xOld,yOld,dx,dy);
 				}
-				int xNew = dx * (colorIndex % 16);
-				int yNew = dy * (colorIndex / 16);
+				int xNew = 1 + dx * (colorIndex % 16);
+				int yNew = 1 + dy * (colorIndex / 16);
 				drawChosenRectFrame(g,xNew,yNew);
 			} 
 		}
---
+++ wextra/source/ui/CCPalette.java
@@ -0,0 +1,85 @@
+package org.concord.waba.extra.ui;
+
+import extra.util.Maths;
+
+public class CCPalette{
+public static waba.fx.Color	[]colors = null;
+public static int CC_VERSION_PALETTE = 0xCC;
+int	paletteVersion = CC_VERSION_PALETTE;
+	private CCPalette(){
+
+	}
+	
+	private static void createPalette(){
+		int r,g,b,i;
+		int count = 0;
+		if(colors == null || colors.length != 216){
+			colors = new waba.fx.Color[216];
+		}
+		for(r = 0; r <=255; r += 51){
+			for(g = 0; g <=255; g += 51){
+				for(b = 0; b <=255; b += 51){
+					colors[count++] = new waba.fx.Color(r,g,b);
+				}
+			}
+		}
+	}
+	
+	public int getPaletteVersion(){return paletteVersion;}
+	
+	public static waba.fx.Color[] getPalette(){
+		if(colors != null) return colors;
+		createPalette();
+		return colors;
+	}
+	
+	public static int findNearestColor(int r, int g, int b){
+		int index = -1;
+		waba.fx.Color	[]cPal = getPalette();
+		if(cPal == null) return index;
+		int minOffset = 0xFFFF;
+		for(int i = 0; i < cPal.length; i++){
+			int tOffset = (Maths.abs(cPal[i].getRed() - r) +Maths.abs(cPal[i].getGreen() - g)+Maths.abs(cPal[i].getBlue() - b));
+			if(tOffset < minOffset){
+				index = i;
+				minOffset = tOffset;
+			}
+		}
+		return index;
+	}
+	public static int findNearestColor(waba.fx.Color col){
+		return findNearestColor(col.getRed(), col.getGreen(), col.getBlue());
+	}
+	public static int findNearestColor(int col){
+		int blue 	= (col & 0xFF);	
+		int green 	= ((col >> 8) & 0xFF);	
+		int red 	= ((col >> 16) & 0xFF);	
+//		System.out.println("red "+ red + " green " + green + " blue " + blue);
+		
+		int index = findNearestColor(red, green, blue);
+/*
+		if(index >= 0){
+			System.out.println("red "+ colors[index].getRed() + " green " + colors[index].getGreen() + " blue " + colors[index].getBlue());
+		}
+*/
+		return index;
+	}
+	
+	
+	public static int []getPaletteAsInt(){
+		int cmap[] = null;
+		waba.fx.Color	[]cPal = getPalette();
+		if(cPal == null) return cmap;
+		int numColors = cPal.length;
+		cmap = new int[numColors];
+
+		for (int i = 0; i < numColors; i++){
+			byte red 	= (byte)cPal[i].getRed();
+			byte green 	= (byte)cPal[i].getGreen();
+			byte blue 	= (byte)cPal[i].getBlue();
+			cmap[i] = ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF);
+		}
+		return cmap;
+	}
+
+}
