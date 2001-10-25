23dd8f7ea38b6993a645f5946cd9ea334cf5c5af
scytacki <>
2001-10-25 15:47:03 +0000

    Fixing formating of menus, and attempted to add remove to Menu, this isn't easy

--- wextra/source/ui/desktop/Menu.java
+++ wextra/source/ui/desktop/Menu.java
@@ -1,121 +1,62 @@
 /*****************************************************************************
-
  *                                Waba Extras
-
  *
-
  * Version History
-
  * Date                Version  Programmer
-
  * ----------  -------  -------  ------------------------------------------
-
  * 08/05/1999  New      0.9.0    Stefan Kellner
-
  * Class created
-
  *
-
  ****************************************************************************/
 
-
-
 package org.concord.waba.extra.ui;
-
 import org.concord.waba.extra.event.*;
-
 import waba.ui.*;
-
 public class Menu implements java.awt.event.ActionListener
-
 {
-
-java.awt.Menu menu;
-
+	java.awt.Menu menu;
     ActionListener actionListener = null;
-
-  public Menu()
-
-  {
-
-	this("");
-
-  }
-
-  public Menu(String label)
-
-  {
-
-	menu = new java.awt.Menu(label);
-
-	menu.addActionListener(this);
-
-  }
-
-
-
+	public Menu()
+	{
+		this("");
+	}
+	public Menu(String label)
+	{
+		menu = new java.awt.Menu(label);
+		menu.addActionListener(this);
+	}
 
 
     public void setName(String name) {
-
 	    menu.setLabel(name);
-
     }
-
     public String getName() {
-
         return menu.getLabel();
-
     }
 
-
-
-  /**
-
-   * Add a new item to the end of the menu. The first item defines
-
-   * the name of the menu. To insert a separator simply add an option 
-
-   * "-" to the menu.
-
-   * @param name the item to add
-
-   */
-
-  public void add(String nameItem)
-
-  {
-
-    menu.add(nameItem);
-
-  }
-
+	/**
+	 * Add a new item to the end of the menu. The first item defines
+	 * the name of the menu. To insert a separator simply add an option 
+	 * "-" to the menu.
+	 * @param name the item to add
+	 */
+	public void add(String nameItem)
+	{
+		menu.add(nameItem);
+	}
   
-
-  public java.awt.Menu getAWTMenu() {return menu;}
-
-  public void 	addActionListener(ActionListener l){
-
-  	if(actionListener == null){
-
-  		actionListener = l;
-
-  	}
-
-  }
-
-  public void 	removeActionListener(ActionListener l){
-
-  	if(actionListener == l){
-
-  		actionListener = null;
-
-  	}
-
-  }
-
+	public java.awt.Menu getAWTMenu() {return menu;}
+	public void 	addActionListener(ActionListener l){
+		if(actionListener == null){
+			actionListener = l;
+		}
+	}
+	public void 	removeActionListener(ActionListener l){
+		if(actionListener == l){
+			actionListener = null;
+		}
+	}
 	public void actionPerformed(java.awt.event.ActionEvent e){
-
 		if(actionListener != null){
 			actionListener.actionPerformed(new ActionEvent(this,null,e.getActionCommand()));
 			MainWindow mw = MainWindow.getMainWindow();
@@ -123,8 +64,5 @@ java.awt.Menu menu;
 			    ((ExtraMainWindow) mw)._finishAWTEvent();
 			}
 		}
-
 	}
-
-
 }
