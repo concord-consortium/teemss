716a5ae5a5017b803d9b8a4aaac4c6388b38665d
scytacki <>
2001-10-26 16:28:47 +0000

    MainView of LabBook

---
+++ LabBook/source/MainView.java
@@ -0,0 +1,15 @@
+package org.concord.LabBook;
+
+import org.concord.waba.extra.ui.*;
+import org.concord.waba.extra.event.*;
+
+public interface MainView
+{
+    public void addMenu(LabObjectView source, Menu menu);
+
+    public void delMenu(LabObjectView source, Menu menu);
+
+	public void addFileMenuItems(String [] items, ActionListener source);
+
+	public void removeFileMenuItems(String [] items, ActionListener source);
+}
