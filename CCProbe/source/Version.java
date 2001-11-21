bd0fbec883cb460ab43119164d2d27cb028143ea
dima <>
2001-11-21 18:24:29 +0000

    build checkin

--- CCProbe/source/Version.java
+++ CCProbe/source/Version.java
@@ -19,7 +19,7 @@ package 	org.concord.CCProbe;
 
 public class Version
 {
-static public int BUILD 	  = 65;
+static public int BUILD 	  = 67;
 static public int VERSION   = 0;
 static public int MVERSION1 = 6;
 static public int MVERSION2 = 5;
