8350c59d66ffc34562023a17449bf87e7843331b
scytacki <>
2001-12-11 17:45:46 +0000

    New interface used by transforms

---
+++ CCProbe/source/DataSink.java
@@ -0,0 +1,19 @@
+package org.concord.CCProbe;
+
+import waba.util.*;
+import waba.ui.*;
+import extra.io.*;
+import extra.util.*;
+import org.concord.waba.extra.probware.Transform;
+import org.concord.waba.extra.probware.CCInterfaceManager;
+
+import org.concord.waba.extra.ui.*;
+import extra.ui.*;
+import extra.util.CCUnit;
+import org.concord.waba.extra.event.*;
+
+public interface DataSink
+{
+	public void addDataSource(DataSource ds);
+	public void removeDataSource(DataSource ds);
+}
