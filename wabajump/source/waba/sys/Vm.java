d8eb9a7c58a28d01fed2aa45b71b4f331b8a6662
scytacki <>
2002-01-21 02:40:15 +0000

    added pageup and pagedown, and started merging Vm

--- wabajump/source/waba/sys/Vm.java
+++ wabajump/source/waba/sys/Vm.java
@@ -38,169 +38,172 @@ import palmos.*;
 
 public class Vm
 {
-private Vm()
+	private Vm()
 	{
 	}
 
-/**
- * Copies the elements of one array to another array. This method returns
- * true if the copy is successful. It will return false if the array types
- * are not compatible or if either array is null. If the length parameter
- * would cause the copy to read or write past the end of one of the arrays,
- * an index out of range error will occur. If false is returned then no
- * copying has been performed.
- * @param srcArray the array to copy elements from
- * @param srcStart the starting position in the source array
- * @param dstArray the array to copy elements to
- * @param dstStart the starting position in the destination array
- * @param length the number of elements to copy
- */
-public static boolean copyArray(Object srcArray, int srcStart,
-	Object dstArray, int dstStart, int length)
-{
-   if(srcArray==null || dstArray==null){
-      return false;
-   }
-   else if((srcArray == dstArray) && 
-	   (srcStart < dstStart)){
-       int step = dstStart - srcStart;
-       int end = srcStart+length;
-       for(int i=end-step; i >= srcStart; i-= step){
-	   System.arraycopy(srcArray,i,srcArray,i+step,step);
-       }
-       return true;
-   } else {       
-      System.arraycopy(srcArray,srcStart,dstArray,dstStart,length);
-      return true;
-   }
-}
-
-
-
-/**
- * Returns true if the system supports a color display and false otherwise.
- */
-public static boolean isColor()
-{
-  return false;
-}
-
-private static int tps;
-
-/**
- * Returns a time stamp in milliseconds. The time stamp is the time
- * in milliseconds since some arbitrary starting time fixed when
- * the VM starts executing. The maximum time stamp value is (1 << 30) and
- * when it is reached, the timer will reset to 0 and will continue counting
- * from there.
- */
-public static int getTimeStamp()
-{
-  if (tps == 0)
-    tps = 100;//Palm.SysTicksPerSecond();
-  // NOTE: Timestamp must be in range from 0..(1 << 30)
-  long stamp = (long)((Palm.TimGetTicks() * 1000L) / tps);
-  return (int)(stamp % (1L << 30));
-}
-
-/** Returns the platform the Virtual Machine is running under as a string. */
-public static String getPlatform()
-{
-  return "PalmOS";
-}
-
-
-/**
- * Returns the username of the user running the Virutal Machine. Because of
- * Java's security model, this method will return null when called in a Java
- * applet. This method will also return null under most WinCE devices (that
- * will be fixed in a future release).
- */
-public static String getUserName()
-{
-  return "Rob Nielsen";
-}
-
-/**
- * Returns the version of the Waba Virtual Machine. The major version is
- * base 100. For example, version 1.0 has value 100. Version 2.0 has a
- * version value of 200. A beta 0.8 VM will have version 80.
- */
-public static int getVersion()
+	/**
+	 * Copies the elements of one array to another array. This method returns
+	 * true if the copy is successful. It will return false if the array types
+	 * are not compatible or if either array is null. If the length parameter
+	 * would cause the copy to read or write past the end of one of the arrays,
+	 * an index out of range error will occur. If false is returned then no
+	 * copying has been performed.
+	 * @param srcArray the array to copy elements from
+	 * @param srcStart the starting position in the source array
+	 * @param dstArray the array to copy elements to
+	 * @param dstStart the starting position in the destination array
+	 * @param length the number of elements to copy
+	 */
+	public static boolean copyArray(Object srcArray, int srcStart,
+									Object dstArray, int dstStart, int length)
 	{
-	return 100;
+		if(srcArray==null || dstArray==null){
+			return false;
+		}
+		else if((srcArray == dstArray) && 
+				(srcStart < dstStart)){
+			int step = dstStart - srcStart;
+			int end = srcStart+length;
+			for(int i=end-step; i >= srcStart; i-= step){
+				System.arraycopy(srcArray,i,srcArray,i+step,step);
+			}
+			return true;
+		} else {       
+			System.arraycopy(srcArray,srcStart,dstArray,dstStart,length);
+			return true;
+		}
 	}
 
-/**
- * Executes a command.
- * <p>
- * As an example, the following call could be used to run the command
- * "scandir /p mydir" under Java, Win32 or WinCE:
- * <pre>
- * int result = Vm.exec("scandir", "/p mydir", 0, true);
- * </pre>
- * This example executes the Scribble program under PalmOS:
- * <pre>
- * Vm.exec("Scribble", null, 0, false);
- * </pre>
- * This example executes the web clipper program under PalmOS, telling
- * it to display a web page by using launchCode 54 (CmdGoToURL).
- * <pre>
- * Vm.exec("Clipper", "http://www.yahoo.com", 54, true);
- * </pre>
- * The args parameter passed to this method is the arguments string
- * to pass to the program being executed.
- * <p>
- * The launchCode parameter is only used under PalmOS. Under PalmOS, it is
- * the launch code value to use when the Vm calls SysUIAppSwitch().
- * If 0 is passed, the default launch code (CmdNormalLaunch) is used to
- * execute the program.
- * <p>
- * The wait parameter passed to this method determines whether to execute
- * the command asynchronously. If false, then the method will return without
- * waiting for the command to complete execution. If true, the method will
- * wait for the program to finish executing and the return value of the
- * method will be the value returned from the application under Java, Win32
- * and WinCE.
- * <p>
- * Under PalmOS, the wait parameter is ignored since executing another
- * program terminates the running program.
- *
- * @param command the command to execute
- * @param args command arguments
- * @param launchCode launch code for PalmOS applications
- * @param wait whether to wait for the command to complete execution before returning
- */
-public static int exec(String command, String args, int launchCode, boolean wait)
-{
-  return 0;
-}
 
 
-/**
- * Sets the device's "auto-off" time. This is the time in seconds where, if no
- * user interaction occurs with the device, it turns off. To keep the device always
- * on, pass 0. This method only works under PalmOS. The integer returned is
- * the previous auto-off time in seconds.
- */
-public static int setDeviceAutoOff(int seconds)
-{
-  return Palm.SysSetAutoOffTime(seconds);
-}
+	/**
+	 * Returns true if the system supports a color display and false otherwise.
+	 */
+	public static boolean isColor()
+	{
+		return false;
+	}
 
-/**
- * Causes the VM to pause execution for the given number of milliseconds.
- * @param millis time to sleep in milliseconds
- */
-public static void sleep(int millis)
-{
-  Palm.SysTaskDelay(millisToTicks(millis));
-}
+	/** Stores the real number of ticks per second in the device or emulator */
+	static int sysTicksPerSecond = 100;  
+	// should use this but I haven't added to Pilot.inc yet Palm.SysTicksPerSecond();
 
-static int millisToTicks(int millis)
-{
-  if (tps == 0)
-    tps = 100;//Palm.SysTicksPerSecond();
-  return millis*tps/1000;
-}
+	/**
+	 * Returns a time stamp in milliseconds. The time stamp is the time
+	 * in milliseconds since some arbitrary starting time fixed when
+	 * the VM starts executing. The maximum time stamp value is (1 << 30) and
+	 * when it is reached, the timer will reset to 0 and will continue counting
+	 * from there.
+	 */
+	public static int getTimeStamp()
+	{
+		// NOTE: Timestamp must be in range from 0..(1 << 30)
+		long stamp = (long)((Palm.TimGetTicks() * 1000L) / sysTicksPerSecond);
+		return (int)(stamp % (1L << 30));
+	}
+
+	/** Returns the platform the Virtual Machine is running under as a string. */
+	public static String getPlatform()
+	{
+		return "PalmOS";
+	}
+
+
+	/**
+	 * Returns the username of the user running the Virutal Machine. Because of
+	 * Java's security model, this method will return null when called in a Java
+	 * applet. This method will also return null under most WinCE devices (that
+	 * will be fixed in a future release).
+	 */
+	public static String getUserName()
+	{
+		/*
+		StringBuffer userName = new StringBuffer (50);
+		int err = Palm.DlkGetSyncInfoUserName (0, 0, 0, userName, 0, 0);
+		return userName.toString();
+		*/
+		return "Scott Cytacki";
+	}
+
+	/**
+	 * Returns the version of the Waba Virtual Machine. The major version is
+	 * base 100. For example, version 1.0 has value 100. Version 2.0 has a
+	 * version value of 200. A beta 0.8 VM will have version 80.
+	 */
+	public static int getVersion()
+	{
+		return 100;
+	}
+
+	/**
+	 * Executes a command.
+	 * <p>
+	 * As an example, the following call could be used to run the command
+	 * "scandir /p mydir" under Java, Win32 or WinCE:
+	 * <pre>
+	 * int result = Vm.exec("scandir", "/p mydir", 0, true);
+	 * </pre>
+	 * This example executes the Scribble program under PalmOS:
+	 * <pre>
+	 * Vm.exec("Scribble", null, 0, false);
+	 * </pre>
+	 * This example executes the web clipper program under PalmOS, telling
+	 * it to display a web page by using launchCode 54 (CmdGoToURL).
+	 * <pre>
+	 * Vm.exec("Clipper", "http://www.yahoo.com", 54, true);
+	 * </pre>
+	 * The args parameter passed to this method is the arguments string
+	 * to pass to the program being executed.
+	 * <p>
+	 * The launchCode parameter is only used under PalmOS. Under PalmOS, it is
+	 * the launch code value to use when the Vm calls SysUIAppSwitch().
+	 * If 0 is passed, the default launch code (CmdNormalLaunch) is used to
+	 * execute the program.
+	 * <p>
+	 * The wait parameter passed to this method determines whether to execute
+	 * the command asynchronously. If false, then the method will return without
+	 * waiting for the command to complete execution. If true, the method will
+	 * wait for the program to finish executing and the return value of the
+	 * method will be the value returned from the application under Java, Win32
+	 * and WinCE.
+	 * <p>
+	 * Under PalmOS, the wait parameter is ignored since executing another
+	 * program terminates the running program.
+	 *
+	 * @param command the command to execute
+	 * @param args command arguments
+	 * @param launchCode launch code for PalmOS applications
+	 * @param wait whether to wait for the command to complete execution before returning
+	 */
+	public static int exec(String command, String args, int launchCode, boolean wait)
+	{
+		return 0;
+	}
+
+
+	/**
+	 * Sets the device's "auto-off" time. This is the time in seconds where, if no
+	 * user interaction occurs with the device, it turns off. To keep the device always
+	 * on, pass 0. This method only works under PalmOS. The integer returned is
+	 * the previous auto-off time in seconds.
+	 */
+	public static int setDeviceAutoOff(int seconds)
+	{
+		return Palm.SysSetAutoOffTime(seconds);
+	}
+
+	/**
+	 * Causes the VM to pause execution for the given number of milliseconds.
+	 * @param millis time to sleep in milliseconds
+	 */
+	public static void sleep(int millis)
+	{
+		Palm.SysTaskDelay(millisToTicks(millis));
+	}
+
+	static int millisToTicks(int millis)
+	{
+		return millis*sysTicksPerSecond/1000;
+	}
 }
