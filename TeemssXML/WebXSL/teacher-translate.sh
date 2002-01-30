bf0e7dfa80be97eec4067a74e9985dec7c51c90e
scytacki <>
2002-01-30 16:52:25 +0000

    added classpath

--- TeemssXML/WebXSL/teacher-translate.sh
+++ TeemssXML/WebXSL/teacher-translate.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
+CLASSPATH=../jars
+
 for XSL_FILE in skills.xsl teacher_intro.xsl teacher_discuss.xsl teacher_background.xsl teacher_timeline.xsl teacher_toc.xsl
  do
 echo translating $XSL_FILE
-java org.apache.xalan.xslt.Process -in ../unit.xml -xsl $XSL_FILE
+java -cp $CLASSPATH org.apache.xalan.xslt.Process -in ../unit.xml -xsl $XSL_FILE
 done
