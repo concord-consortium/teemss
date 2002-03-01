9b82135e96d6e263eed9924b3e378b59c6a9dce6
scytacki <>
2002-03-01 00:28:55 +0000

    generalized script

--- TeemssXML/WebXSL/translate.sh
+++ TeemssXML/WebXSL/translate.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
+appdir=`dirname $0`
+cd $appdir
+
 for XSL_FILE in analysis.xsl further.xsl hints.xsl intro.xsl investigation_toc.xsl materials.xsl project_toc.xsl safety.xsl think.xsl trial.xsl unit_toc.xsl
  do
 echo translating $XSL_FILE
-java org.apache.xalan.xslt.Process -in ../unit.xml -xsl $XSL_FILE
+java -cp ../jars/bsf.jar:../jars/js.jar:../jars/xalan.jar:../jars/xerces.jar org.apache.xalan.xslt.Process -in ../unit.xml -xsl $XSL_FILE
 done
+
+chmod -R a+w ../html
