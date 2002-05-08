d061e699309c20a6c73d66a3bf35269151087090
scytacki <>
2002-05-08 19:00:58 +0000

    added more translation to fix long file names

--- TeemssXML/PdfXSL/rn-pdfs.sh
+++ TeemssXML/PdfXSL/rn-pdfs.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 
-
-for FO_FILE in `ls ../pdf/Energy*.pdf`
+for FO_FILE in `ls *.pdf`
  do
 echo copying $FO_FILE
-cp $FO_FILE `echo $FO_FILE | sed -e 's/Energy/te/g'`
+mv $FO_FILE `echo $FO_FILE | 
+             sed -e 's/^Energy/te/g' | 
+	     sed -e 's/^Motions_and_Forces/mf/g' |
+	     sed -e 's/Heating_With_Electricity/Heating/g' |
+	     sed -e 's/Multiple_Transformations/Multiple_Trans/g' |
+	     sed -e 's/Single_Transformations/Single_Trans/g' |
+	     sed -e 's/Potential_Kinetic_Energy/Potential_Kinetic/g'`
 done
