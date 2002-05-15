70a9d7c2ec29e39511001a0553502826bc510097
scytacki <>
2002-05-15 15:55:15 +0000

    build checkin

--- CCProbe/source/Version.java
+++ CCProbe/source/Version.java
@@ -19,7 +19,7 @@ package 	org.concord.CCProbe;
 
 public class Version
 {
-static public int BUILD 	  = 166;
+static public int BUILD 	  = 167;
 static public int VERSION   = 0;
 static public int MVERSION1 = 7;
 static public int MVERSION2 = 0;
