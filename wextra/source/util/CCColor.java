38006f3db7c213c363878a06ed8c5f4cf0ea17db
scytacki <>
2002-05-31 13:12:52 +0000

    added new class which is more efficient than old color class

---
+++ wextra/source/util/CCColor.java
@@ -0,0 +1,15 @@
+package org.concord.waba.extra.util;
+
+public class CCColor
+{
+	public short r;
+	public short g;
+	public short b;
+
+	public CCColor(int red, int green, int blue)
+	{
+		r = (short)red;
+		g = (short)green;
+		b = (short)blue;
+	}
+}
