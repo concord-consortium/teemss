12082e0c5dbf010516a5deb0bab0d96921e03f7f
stephen <>
2002-06-13 18:32:47 +0000

    build checkin

--- CCProbe/source/Version.java
+++ CCProbe/source/Version.java
@@ -19,7 +19,7 @@ package 	org.concord.CCProbe;
 
 public class Version
 {
-static public int BUILD 	  = 169;
+static public int BUILD 	  = 170;
 static public int VERSION   = 0;
 static public int MVERSION1 = 7;
 static public int MVERSION2 = 0;
